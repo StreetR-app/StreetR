123c4753a224f45d471277fbb624840c82764eaf
StreetR-app <StreetR143@gmail.com>
2025-12-16 23:41:39 +0530

    Update js_supabase.js

--- js_supabase.js
+++ js_supabase.js
@@ -1,6 +1,6 @@
 //js&supabase.js
-const SUPABASE_URL = 'https://qdmlbedacymowcgtykvm.supabase.co'; // YOUR_SUPABASE_URL
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkbWxiZWRhY3ltb3djZ3R5a3ZtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkyNjMwOTUsImV4cCI6MjA2NDgzOTA5NX0.oQ98WG4hXtlwTUaj5Aj9R6UVHPZvgR0iQnhxuVtVfRc'; // YOUR_SUPABASE_ANON_KEY
+const SUPABASE_URL = 'https://fdtbltfnkbdonzlvcvcg.supabase.co'; // YOUR_SUPABASE_URL
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZkdGJsdGZua2Jkb256bHZjdmNnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU5MDIxMDYsImV4cCI6MjA4MTQ3ODEwNn0._m-JWFrEt2zA2Uj2OQtjI-uGP5QktZFrr7Fm90wdHuI'; // YOUR_SUPABASE_ANON_KEY
 
 if (!SUPABASE_URL || !SUPABASE_ANON_KEY || SUPABASE_URL.includes('YOUR_SUPABASE_URL')) {
     alert("Application is not configured correctly. Supabase credentials missing in js/js_supabase.js");
